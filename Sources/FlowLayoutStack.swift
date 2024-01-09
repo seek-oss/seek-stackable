6eb3e559ee0499fda0ccb3dc77595bf26a3bdd80
Shaun Ervine <servine@seek.com.au>
2024-01-09 21:56:34 +1100

    Add new FlowLayoutStack to enable flow layout esq designs

---
+++ Sources/FlowLayoutStack.swift
@@ -0,0 +1,122 @@
+//  Copyright © 2024 SEEK. All rights reserved.
+//
+
+open class FlowLayoutStack: Stack {
+    public var thingsToStack: [Stackable]
+    public var spacing: CGFloat
+    public var lineSpacing: CGFloat
+    // layoutMargins have not yet been implemented
+    public var layoutMargins: UIEdgeInsets = .zero
+    public var width: CGFloat?
+    private var itemSpacing: CGFloat {
+        spacing
+    }
+    
+    public init(
+        itemSpacing: CGFloat = 0.0,
+        lineSpacing: CGFloat = 0.0,
+        width: CGFloat? = nil,
+        thingsToStack: [Stackable]
+    ) {
+        self.spacing = itemSpacing
+        self.lineSpacing = lineSpacing
+        self.layoutMargins = .zero
+        self.width = width
+        self.thingsToStack = thingsToStack
+    }
+    
+    public convenience init(
+        itemSpacing: CGFloat = 0.0,
+        lineSpacing: CGFloat = 0.0,
+        width: CGFloat? = nil,
+        thingsToStack: () -> [Stackable]
+    ) {
+        self.init(
+            itemSpacing: itemSpacing,
+            lineSpacing: lineSpacing,
+            width: width,
+            thingsToStack: thingsToStack()
+        )
+    }
+    
+    open func framesForLayout(_ width: CGFloat, origin: CGPoint) -> [CGRect] {
+        var frames: [CGRect] = []
+        var currentY = origin.y
+        var currentX = origin.x
+
+        func moveToNextRow() {
+            currentX = 0
+            currentY = frames.reduce(0) { result, rect in
+                max(
+                    result,
+                    rect.origin.y + rect.size.height
+                )
+            } + lineSpacing
+        }
+        
+        for stackable in self.visibleThingsToStack() {
+            let stackableWidth = getWidth(
+                for: stackable,
+                width: width
+            )
+                            
+            if currentX + stackableWidth > width {
+                moveToNextRow()
+            }
+            
+            if let stack = stackable as? Stack {
+                frames.append(
+                    contentsOf: stack.framesForLayout(
+                        (width - currentX),
+                        origin: .init(
+                            x: currentX,
+                            y: currentY
+                        )
+                    )
+                )
+            } else if let item = stackable as? StackableItem {
+                frames.append(
+                    .init(
+                        x: currentX,
+                        y: currentY,
+                        width: stackableWidth,
+                        height: item.heightForWidth(stackableWidth)
+                    )
+                )
+            }
+
+            currentX += stackableWidth + itemSpacing
+        }
+
+        return frames
+    }
+    
+    private func getWidth(for stackable: Stackable, width: CGFloat) -> CGFloat {
+        return if let stackableItem = stackable as? StackableItem {
+            min(
+                stackableItem.intrinsicContentSize.width,
+                width
+            )
+        } else if let fixedSizeStack = stackable as? Stack, let stackWidth = fixedSizeStack.width {
+            stackWidth
+        } else {
+           stackable.intrinsicContentSize.width
+        }
+    }
+    
+    public var intrinsicContentSize: CGSize {
+        let items = visibleThingsToStack()
+        
+        guard !items.isEmpty else { return .zero }
+        
+        // width
+        let totalWidthOfItems = items.reduce(0, { $0 + $1.intrinsicContentSize.width })
+        let totalHorizontalSpacing = max(CGFloat(items.count) - 1, 0) * spacing
+        let intrinsicWidth = totalWidthOfItems + totalHorizontalSpacing
+        
+        // height
+        let intrinsicHeight = items.reduce(0, { max($0, $1.intrinsicContentSize.height) })
+        
+        return CGSize(width: intrinsicWidth, height: intrinsicHeight)
+    }
+}
---
+++ Tests/FlowLayoutStackTests.swift
@@ -0,0 +1,362 @@
+//  Copyright © 2024 SEEK. All rights reserved.
+//
+
+import Foundation
+@testable import stackable
+import XCTest
+
+class FlowLayoutStackTests: XCTestCase {
+    func test_init_should_set_default_properties() {
+        let stack = FlowLayoutStack(
+            thingsToStack: []
+        )
+        
+        XCTAssertEqual(
+            stack.spacing,
+            0.0
+        )
+        XCTAssertEqual(
+            stack.lineSpacing,
+            0.0
+        )
+        XCTAssertEqual(
+            stack.layoutMargins,
+            .zero
+        )
+        XCTAssertNil(
+            stack.width
+        )
+        XCTAssertEqual(
+            stack.thingsToStack.count,
+            0
+        )
+    }
+    
+    func test_init_should_properties() {
+        let stack = FlowLayoutStack(
+            itemSpacing: 5,
+            lineSpacing: 10,
+            width: 100,
+            thingsToStack: [
+                UILabel()
+            ]
+        )
+        
+        XCTAssertEqual(
+            stack.spacing,
+            5
+        )
+        XCTAssertEqual(
+            stack.lineSpacing,
+            10
+        )
+        XCTAssertEqual(
+            stack.layoutMargins,
+            .zero
+        )
+        XCTAssertEqual(
+            stack.width,
+            100
+        )
+        XCTAssertEqual(
+            stack.thingsToStack.count,
+            1
+        )
+    }
+    
+    func test_convenience_init_should_properties() {
+        let stack = FlowLayoutStack(
+            itemSpacing: 5,
+            lineSpacing: 10,
+            width: 100
+        ) {
+            [
+                UILabel()
+            ]
+        }
+        
+        XCTAssertEqual(
+            stack.spacing,
+            5
+        )
+        XCTAssertEqual(
+            stack.lineSpacing,
+            10
+        )
+        XCTAssertEqual(
+            stack.layoutMargins,
+            .zero
+        )
+        XCTAssertEqual(
+            stack.width,
+            100
+        )
+        XCTAssertEqual(
+            stack.thingsToStack.count,
+            1
+        )
+    }
+    
+    func test_framesForLayout_when_thingsToStack_flow_across_multiple_lines_with_different_heights_should_return_expected() {
+        let stack = FlowLayoutStack(
+            itemSpacing: 5,
+            lineSpacing: 10
+        ) {
+            [
+                StackableItemView(
+                    frame: .init(
+                        origin: .zero,
+                        size: CGSize(
+                            width: 100,
+                            height: 10
+                        )
+                    )
+                ),
+                StackableItemView(
+                    frame: .init(
+                        origin: .zero,
+                        size: CGSize(
+                            width: 100,
+                            height: 50
+                        )
+                    )
+                ),
+                StackableItemView(
+                    frame: .init(
+                        origin: .zero,
+                        size: CGSize(
+                            width: 200,
+                            height: 20
+                        )
+                    )
+                )
+            ]
+        }
+
+        let frames = stack.framesForLayout(300)
+
+        XCTAssertEqual(
+            frames,
+            [
+                .init(
+                    origin: .zero,
+                    size: .init(
+                        width: 100,
+                        height: 10
+                    )
+                ),
+                .init(
+                    origin: .init(
+                        x: 105,
+                        y: 0
+                    ),
+                    size: .init(
+                        width: 100,
+                        height: 50
+                    )
+                ),
+                .init(
+                    origin: .init(
+                        x: 0,
+                        y: 60
+                    ),
+                    size: .init(
+                        width: 200,
+                        height: 20
+                    )
+                )
+            ]
+        )
+    }
+      
+    func test_framesForLayout_when_thingsToStack_contain_fixed_stacks_should_return_expected() {
+        let stack = FlowLayoutStack(
+            itemSpacing: 5,
+            lineSpacing: 10
+        ) {
+            [
+                StackableItemView(
+                    frame: .init(
+                        origin: .zero,
+                        size: CGSize(
+                            width: 250,
+                            height: 10
+                        )
+                    )
+                ),
+                HStack(
+                    thingsToStack: [
+                        UIView().fixed(
+                            size: CGSize(
+                                width: 100,
+                                height: 10
+                            )
+                        )
+                    ],
+                    width: 100
+                )
+            ]
+        }
+
+        let frames = stack.framesForLayout(300)
+
+        XCTAssertEqual(
+            frames,
+            [
+                .init(
+                    origin: .zero,
+                    size: .init(
+                        width: 250,
+                        height: 10
+                    )
+                ),
+                .init(
+                    origin: .init(
+                        x: 0,
+                        y: 20
+                    ),
+                    size: .init(
+                        width: 100,
+                        height: 10
+                    )
+                ),
+            ]
+        )
+    }
+    
+    func test_framesForLayout_when_thingsToStack_contain_stacks_should_return_expected() {
+        let stack = FlowLayoutStack(
+            itemSpacing: 5,
+            lineSpacing: 10
+        ) {
+            [
+                StackableItemView(
+                    frame: .init(
+                        origin: .zero,
+                        size: CGSize(
+                            width: 250,
+                            height: 10
+                        )
+                    )
+                ),
+                HStack(
+                    spacing: 8,
+                    thingsToStack: [
+                        UIView().fixed(
+                            size: CGSize(
+                                width: 10,
+                                height: 10
+                            )
+                        ),
+                        StackableItemView(
+                            frame: .init(
+                                origin: .zero,
+                                size: CGSize(
+                                    width: 250,
+                                    height: 10
+                                )
+                            )
+                        )
+                    ]
+                )
+            ]
+        }
+
+        let frames = stack.framesForLayout(300)
+
+        XCTAssertEqual(
+            frames,
+            [
+                .init(
+                    origin: .zero,
+                    size: .init(
+                        width: 250,
+                        height: 10
+                    )
+                ),
+                .init(
+                    origin: .init(
+                        x: 0,
+                        y: 20
+                    ),
+                    size: .init(
+                        width: 10,
+                        height: 10
+                    )
+                ),
+                .init(
+                    origin: .init(
+                        x: 18,
+                        y: 20
+                    ),
+                    size: .init(
+                        width: 282,
+                        height: 10
+                    )
+                ),
+            ]
+        )
+    }
+
+    func test_intrinsicContentSize_should_return_correct_size() {
+        let stack = FlowLayoutStack(
+            itemSpacing: 5
+        ) {
+            [
+                UIView().fixed(
+                    width: 100,
+                    height: 100
+                ),
+                UIView().fixed(
+                    width: 200,
+                    height: 50
+                )
+            ]
+        }
+
+        XCTAssertEqual(
+            stack.intrinsicContentSize,
+            .init(
+                width: 305,
+                height: 100
+            )
+        )
+    }
+
+    func test_intrinsicContentSize_should_return_zero_when_items_are_hidden() {
+        let view1 = UIView()
+        view1.isHidden = true
+        let view2 = UIView()
+        view2.isHidden = true
+
+        let stack = FlowLayoutStack(
+            itemSpacing: 5
+        ) {
+            [
+                view1.fixed(
+                    width: 100,
+                    height: 100
+                ),
+                view2.fixed(
+                    width: 200,
+                    height: 50
+                )
+            ]
+        }
+
+        XCTAssertEqual(
+            stack.intrinsicContentSize,
+            .zero
+        )
+    }
+    
+    private class StackableItemView: UIView, StackableItem {
+        override var intrinsicContentSize: CGSize {
+            frame.size
+        }
+        
+        func heightForWidth(_ width: CGFloat) -> CGFloat {
+            frame.height
+        }
+    }
+}
